0cd297ebcad841124e047e1884921bd980b7d892
Prokhor Krylov <proxoraleksandrovich@gmail.com>
2026-04-21 11:27:19 +0300

    fix: handle missing user with 404 in ProfileApiController

--- src/main/kotlin/ru/proshik/pochitushki/controller/ProfileApiController.kt
+++ src/main/kotlin/ru/proshik/pochitushki/controller/ProfileApiController.kt
@@ -1,11 +1,14 @@
 package ru.proshik.pochitushki.controller
 
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import ru.proshik.pochitushki.model.UserSettingsData
 import ru.proshik.pochitushki.service.UserService
 
@@ -23,7 +26,11 @@ class ProfileApiController(private val userService: UserService) {
         @RequestAttribute("userId") userId: Long,
         @RequestBody patch: SettingsPatchRequest,
     ): ResponseEntity<Void> {
-        val current = userService.getUserByUserId(userId).settings
+        val current = try {
+            userService.getUserByUserId(userId).settings
+        } catch (e: EmptyResultDataAccessException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+        }
         val updated = UserSettingsData(
             languageCode = patch.languageCode ?: current.languageCode,
             tgFeedEntriesNumber = patch.tgFeedEntriesNumber ?: current.tgFeedEntriesNumber,
